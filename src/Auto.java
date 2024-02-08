de6ee4ed80ddc14393817629e932c0f82759c85e
4a <4a@labinf1-04>
2024-02-08 09:24:53 +0100

    Versione 1

---
+++ src/Auto.java
@@ -0,0 +1,32 @@
+/*
+ * versione 1
+ */
+public class Auto extends Veicolo {
+private String tipo;
+private final int tariffaG=10;
+
+	
+	public Auto(String t, String m, boolean dis, float cost, String tip) {
+		super(t, m, dis, cost);
+		tipo=tip;
+		
+	}
+	
+	public float getCostoG() {
+		return tariffaG-costovig;
+	}
+	
+	public String toString() {
+		String s="VEICOLO: AUTO\n";
+		s+=super.toString();
+		s+="Tipo auto: "+tipo+"\n";
+		s+="Tariffa giornarliera: "+tariffaG+"€\n";
+		return s;
+	}
+	
+	public static void main(String[] args) {
+		Auto a=new Auto("FA903AZ","Nissan",true,45,"Utilitaria");
+		System.out.println(a);
+	}
+
+}
